f85e8d0b38287fc0ad86d3cbb81113e3a581bb2c
Shridhar Sharma <shridharkaushik29@gmail.com>
2018-07-18 10:19:52 +0530

    Removed the bug
    In previous version publish command has to be run via administrator in windows, now it is not required.
    Added a bower:link artisan route to link bower_components directory

--- src/routes.php
+++ src/routes.php
@@ -2,9 +2,21 @@
 
 use Illuminate\Support\Facades\Artisan;
 use Shridhar\Bower\Bower;
+use Illuminate\Filesystem\Filesystem;
 
 Artisan::command("bower:install {name} {--path=}", function($name, $path = null) {
     Bower::make()->getComponent($name)->install();
+});
+
+Artisan::command("bower:link", function($name, $path = null) {
+    $path = base_path("bower_components");
+    $link = public_path("bower_components");
+    if (!file_exists($path)) {
+        mkdir($path);
+    }
+    $fs = new Filesystem();
+    $fs->link($path, $link);
+    $this->info("bower_components directory linked to public directory");
 });
 
 Artisan::command("bower:uninstall {name} {--path=}", function($name, $path = null) {
